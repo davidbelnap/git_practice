41abc4fc0a0e48e711bf826e6195555f5959bd79
David Belnap <david.belnap6@gmail.com>
2017-08-16 11:35:09 -0600

    Completed Git Command program

--- main.rb
+++ main.rb
@@ -1,7 +1,24 @@
 
-puts "Learning Git"
+def puts_git(cmd)
+	puts `git #{cmd} -h`
+end
 
-first_name = "David"
-last_name = "Belnap"
+def menu
+	puts '1: Enter Git Command'
+	puts '2: Exit'
+	choice = gets.to_i
+	case choice
+		when 1
+			puts "Enter a git command."
+			puts_git(gets.strip)
+		when 2
+			puts "Thanks for using the Git Command!"
+			exit
+		else
+			puts "Please enter a valid selection (1 or 2)."
+			menu
+	end
+	menu
+end
 
-puts "Hello #{first_name} #{last_name}"
+menu
